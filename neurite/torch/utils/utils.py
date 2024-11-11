7f7a5fca40821b58c98b3fd7535877faaa6ee35b
Etienne Chollet <etiennepchollet@gmail.com>
2024-11-10 20:53:52 -0500

    [DOCS] Demonstrate visualization Examples of soft_quantize()

--- neurite/torch/utils/utils.py
+++ neurite/torch/utils/utils.py
@@ -71,11 +71,15 @@ def soft_quantize(
 
     Examples
     --------
-    >>> # Make a random 3D tensor with zero mean and unit variance.
+    >>> import torch
+    >>> import matplotlib.pyplot as plt
+    # Make a random 3D tensor with zero mean and unit variance.
     >>> input_tensor = torch.randn(1, 1, 32, 32, 32)
-    # Compute the softly quantized tensor with a low softness to approximate (and visualize) a
-    # pseudo-hard quantization.
+    # Compute the softly quantized tensor with a low softness to approximate (and visualize) a 
+    # pseudo-hard quantization. 
     >>> softly_quantized_tensor = soft_quantize(input_tensor, nb_bins=4, softness=0.0001)
+    # Visualize the softly quantized tensor.
+    >>> plt.imshow(x[0, 0, 16])
     """
     # Invert softness
     softness = 1 / softness
