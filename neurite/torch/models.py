afcfd69b5627d3679662c0d71eaba18149bd1eb7
Etienne Chollet <etiennepchollet@gmail.com>
2025-01-06 21:26:02 -0500

    [FEAT] Introduce FlexibleUNet skeleton/

--- neurite/torch/models.py
+++ neurite/torch/models.py
@@ -3,3 +3,58 @@ Prebuilt yet flexible neural network architectures designed for specific tasks, 
 segmentation, registration, or classification. models leverage layers and modules from other
 components of the neurite for streamlined object construction.
 """
+__all__ = [
+    "FlexibleUNet"
+]
+
+import torch
+from torch import nn
+
+
+class FlexibleUNet(nn.Module):
+    """
+    Flexible unet with many configuration options!
+
+    Attributes
+    ----------
+
+    Notes
+    -----
+
+    Examples
+    --------
+    """
+
+    def __init__(self):
+        """
+        Instantiate `FlexibleUNet`
+
+        Parameters
+        ----------
+
+        Raises
+        ------
+
+        """
+        super().__init__()
+        raise NotImplementedError(
+            "FlexibleUNet is not yet ready :("
+        )
+
+    def forward(self, input_tensor: torch.Tensor):
+        """
+        Forward pass through the FlexibleUNet model.
+
+        Parameters
+        ----------
+        input_tensor : torch.Tensor
+            Tensor to be passed through the model. Assumed to have batch and channel dimensions.
+
+        Returns
+        -------
+        torch.Tensor
+            Result of forward pass of the model.
+        """
+        raise NotImplementedError(
+            "forward() method of FlexibleUNet"
+        )
